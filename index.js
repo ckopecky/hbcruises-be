d7a8c4028cc0daa3360c855b1822876ef1f6db9d
ckopecky <kopecky12112@gmail.com>
2020-11-20 01:51:46 -0800

    Add WeatherAPI class RESTDataSource

--- index.js
+++ index.js
@@ -4,3 +4,19 @@ require('dotenv').config();
 const { ApolloServer, gql } = require('apollo-server');
 const { RESTDataSource } = require('apollo-datasource-rest');
 
+//REST DataSource
+
+class WeatherAPI extends RESTDataSource {
+    constructor(postalCode) {
+        super();
+        this.baseURL="http://api.openweathermap.org/data/2.5/"
+        this.postalCode = postalCode
+    }
+
+    async getWeather(postalCode) {
+        return this.get(`weather?zip=63701&units=imperial&appid=${process.env.API_KEY}`)
+    }
+}
+
+
+
