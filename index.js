36f174e686a6e735ef76708380f375dd9166e146
ckopecky <kopecky12112@gmail.com>
2020-11-20 23:25:25 -0800

    add weather API

--- index.js
+++ index.js
@@ -7,15 +7,18 @@ const { RESTDataSource } = require('apollo-datasource-rest');
 //REST DataSource
 
 class WeatherAPI extends RESTDataSource {
-    constructor(postalCode) {
-        super();
-        this.baseURL="http://api.openweathermap.org/data/2.5/"
-        this.postalCode = postalCode
-    }
+	constructor() {
+		super();
+		this.baseURL = 'http://api.openweathermap.org/data/2.5/';
+	}
 
-    async getWeather(postalCode) {
-        return this.get(`weather?zip=63701&units=imperial&appid=${process.env.API_KEY}`)
-    }
+	async getWeather(postalCode) {
+		console.log('get', postalCode.zip);
+		let response = await this.get(
+			`weather?zip=${postalCode.zip}&units=imperial&appid=${process.env.API_KEY}`
+		);
+		return response;
+	}
 }
 
 
